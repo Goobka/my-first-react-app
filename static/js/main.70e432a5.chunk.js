dda22d04dbce5cda66f196b12d9ca24d9b7643a4
Oleksandra Peleno <alexandragubar@gmail.com>
2021-03-21 10:19:03 +0200

    Updates

---
+++ static/js/main.70e432a5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-first-react-app"]=this["webpackJsonpmy-first-react-app"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);c(1);var r=c(3),s=c.n(r),n=c(0);var a=function(){return Object(n.jsx)("div",{className:"App",children:Object(n.jsx)("h1",{children:"Hello React!"})})};s.a.render(Object(n.jsx)(a,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.70e432a5.chunk.js.map
